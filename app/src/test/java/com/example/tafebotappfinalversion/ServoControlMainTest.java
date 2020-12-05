5f381f29730a90f588d192281546618b91e665d9
Virroshan <virroshan08@outlook.com>
2020-12-06 03:05:17 +1030

    Junit testing comit

--- app/src/test/java/com/example/tafebotappfinalversion/ServoControlMainTest.java
+++ app/src/test/java/com/example/tafebotappfinalversion/ServoControlMainTest.java
@@ -1,5 +1,8 @@
 package com.example.tafebotappfinalversion;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
 import android.widget.Button;
 
 import org.junit.After;
@@ -15,6 +18,7 @@ import androidx.test.core.app.ActivityScenario;
 import java.io.IOException;
 
 import static java.util.regex.Pattern.matches;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
@@ -31,23 +35,39 @@ public class ServoControlMainTest {
 //        // Your test code goes here.
 //    }
 
+        @Test
+    public void testIntent(){
+        assertTrue("true != true", true);
+        assertFalse("false != false", false);
+
+        final String extraName = "IamAnExtra";
+        final String extraValue = "IamAValue";
+        Intent intent = new Intent();
+        intent.putExtra(extraName, extraValue);
+
+        assertTrue("hasExtra==false", intent.hasExtra(extraName));
+    }
+
+    @Test
+    public void activityResult_DisplaysContactsPhoneNumber() {
+        // Build the result to return when the activity is launched.
+        Intent resultData = new Intent();
+        String phoneNumber = "123-345-6789";
+        resultData.putExtra("phone", phoneNumber);
+        Instrumentation.ActivityResult result =
+                new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+
+        // Set up result stubbing when an intent sent to "contacts" is seen.
+        intending(toPackage("com.android.contacts")).respondWith(result);
+
+        // User action that results in "contacts" activity being launched.
+        // Launching activity expects phoneNumber to be returned and displayed.
+        onView(withId(R.id.pickButton)).perform(click());
+
+        // Assert that the data we set up above is shown.
+        onView(withId(R.id.phoneNumber)).check(matches(withText(phoneNumber)));
+    }
 
 
-
-//        @Rule
-//        public ActivityScenarioRule = new ActivityScenarioRule<>(ServoControlMain.class);
-//
-//        @Before
-//        public void initValidString() {
-//            // Specify a valid string.
-//            stringToBetyped = "up";
-//        }
-//
-//        @Test
-//        public void changeText_sameActivity() {
-//            // Type text and then press the button.
-//           OnView(withId(R.id.servo1ButtonOpen))
-//           .check(matches(withText(stringToBetyped)));
-//        }
     }
 
